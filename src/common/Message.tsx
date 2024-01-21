cbd9bf04a6d62552b10ccc3980ec309466ee775a
tyranoboy1 <gkwlgnd@naver.com>
2024-01-22 00:08:59 +0900

    [UPDATE] 로딩, 에러 메세지 컴포넌트

--- src/common/Message.tsx
+++ src/common/Message.tsx
@@ -5,7 +5,67 @@ import classNames from 'classnames/bind'
 const cx = classNames.bind(styles)
 /** 로딩메세지, 에러메세지 컴포넌트 */
 const Message = ({ type }: IMessage) => {
-  return <div className={cx('container')}>dfdfdfdfdfdf</div>
+  return (
+    <div className={cx('container')}>
+      {type === 'loading' ? (
+        <Heart />
+      ) : (
+        <>
+          <Error />
+          에러가 발생했어요 잠시후 다시 시도해주세요
+        </>
+      )}
+    </div>
+  )
 }
 
+// 하트모양 이미지 컴포넌트
+const Heart = () => {
+  return (
+    <svg
+      className={cx('icon-heart')}
+      version="1.1"
+      viewBox="0 0 512 512"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g id="_x31_66_x2C__Heart_x2C__Love_x2C__Like_x2C__Twitter">
+        <g>
+          <path d="M365.4,59.628c60.56,0,109.6,49.03,109.6,109.47c0,109.47-109.6,171.8-219.06,281.271    C146.47,340.898,37,278.568,37,169.099c0-60.44,49.04-109.47,109.47-109.47c54.73,0,82.1,27.37,109.47,82.1    C283.3,86.999,310.67,59.628,365.4,59.628z" />
+        </g>
+      </g>
+      <g id="Layer_1" />
+    </svg>
+  )
+}
+// 에러 모양 이미지 컴포넌트
+const Error = () => {
+  return (
+    <svg
+      className={cx('icon-error')}
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g transform="translate(0 -1028.4)">
+        <path
+          d="m22 12c0 5.523-4.477 10-10 10-5.5228 0-10-4.477-10-10 0-5.5228 4.4772-10 10-10 5.523 0 10 4.4772 10 10z"
+          fill="#c0392b"
+          transform="translate(0 1029.4)"
+        />
+        <path
+          d="m22 12c0 5.523-4.477 10-10 10-5.5228 0-10-4.477-10-10 0-5.5228 4.4772-10 10-10 5.523 0 10 4.4772 10 10z"
+          fill="#e74c3c"
+          transform="translate(0 1028.4)"
+        />
+        <path
+          d="m7.0503 1037.8 3.5357 3.6-3.5357 3.5 1.4142 1.4 3.5355-3.5 3.536 3.5 1.414-1.4-3.536-3.5 3.536-3.6-1.414-1.4-3.536 3.5-3.5355-3.5-1.4142 1.4z"
+          fill="#c0392b"
+        />
+        <path
+          d="m7.0503 1036.8 3.5357 3.6-3.5357 3.5 1.4142 1.4 3.5355-3.5 3.536 3.5 1.414-1.4-3.536-3.5 3.536-3.6-1.414-1.4-3.536 3.5-3.5355-3.5-1.4142 1.4z"
+          fill="#ecf0f1"
+        />
+      </g>
+    </svg>
+  )
+}
 export default Message
