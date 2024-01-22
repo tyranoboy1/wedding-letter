5ed7f2deb11040504536bc234d34753ae8471fa2
tyranoboy1 <gkwlgnd@naver.com>
2024-01-23 00:39:07 +0900

    [UPDATE] main video 추가

--- src/components/section/Video.tsx
+++ src/components/section/Video.tsx
@@ -5,7 +5,22 @@ import styles from './style/heading.module.scss'
 //TODO vido태그 작업
 const cx = classNames.bind(styles)
 const Video = () => {
-  return <SectionLayout>Heading</SectionLayout>
+  return (
+    <SectionLayout className={cx('container')}>
+      <video
+        autoPlay
+        muted
+        loop
+        controls
+        poster={require('../../assets/poster.jpg')}
+      >
+        <source
+          src={require('../../assets/main.mp4')}
+          type="video/mp4"
+        ></source>
+      </video>
+    </SectionLayout>
+  )
 }
 
 export default Video
